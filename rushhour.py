3341ac235f70bdad86538bcd16e8715fd015622f
Britt van Buuren <brittvanbuuren@gmail.com>
2021-01-08 12:02:22 +0100

    making a solution file

--- rushhour.py
+++ rushhour.py
@@ -1,17 +1,38 @@
+import csv
+from pathlib import Path
+
 from board import Cars, Board
 
 class Solve_game:
     def __init__(self, game):
+        self.game = game
 
+    def load_game(self): 
         board = Board()
-        if game <= 3: 
-            board.load_cars(f"boards/Rushhour6x6_{game}.csv")
-        elif game <= 6: 
-            board.load_cars(f"boards/Rushhour9x9_{game}.csv")
+        if self.game <= 3: 
+            board.load_cars(f"boards/Rushhour6x6_{self.game}.csv")
+        elif self.game <= 6: 
+            board.load_cars(f"boards/Rushhour9x9_{self.game}.csv")
         else: 
-            board.load_cars(f"boards/Rushhour12x12_{game}.csv")
+            board.load_cars(f"boards/Rushhour12x12_{self.game}.csv")
         
-        board.load_board()
+        return board.load_board()
+    
+    def move(self): 
+        pass
+
+    def solution(self): 
+        i = 0 
+        
+        while True:  
+            my_file = Path(f'solutions/solution_game_{self.game}.{i}.csv')
+            if my_file.is_file():
+                i += 1
+            else: break
+
+        with open(f'solutions/solution_game_{self.game}.{i}.csv', 'w') as csv_file:
+            writer = csv.writer(csv_file)
+            writer.writerow(['car', 'move']) 
 
 
 if __name__ == "__main__":
@@ -32,3 +53,6 @@ if __name__ == "__main__":
     print("Welcome to Rush Hour!\n")
 
     solve_game = Solve_game(game_name)
+    solve_game.load_game()
+
+    solve_game.solution()
