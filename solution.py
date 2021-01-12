fd7cc2ea9fcdda1d28bca52e8fa085d27d34dde1
Sneeuwmans <70612497+Sneeuwmans@users.noreply.github.com>
2021-01-12 13:35:56 +0100

    Update solution.py

--- solution.py
+++ solution.py
@@ -6,14 +6,20 @@ class Solution:
 
     def __init__(self, game):
         self.game = game
-
-    def load_game(self): 
-        board = Board()
-        if self.game <= 3: 
-            board.load_cars(f"boards/Rushhour6x6_{self.game}.csv")
-        elif self.game <= 6: 
-            board.load_cars(f"boards/Rushhour9x9_{self.game}.csv")
-        else: 
-            board.load_cars(f"boards/Rushhour12x12_{self.game}.csv")
         
-        return board.load_board()
+    def solution(self): 
+        i = 0 
+
+        test_solution = [ ['A', '1'], ['C', '-2'], ['G', '-1']]
+        
+        while True:  
+            my_file = Path(f'solutions/solution_game_{self.game}.{i}.csv')
+            if my_file.is_file():
+                i += 1
+            else: break
+
+        with open(f'solutions/solution_game_{self.game}.{i}.csv', 'w') as csv_file:
+            writer = csv.writer(csv_file)
+            writer.writerow(['car', 'move']) 
+            writer.writerows(test_solution)
+            
