4b8ffc47653cb2c0b62fe92ead9a4068ddbbe994
Luuk Engele <lengele489@LaptopLuuk.localdomain>
2021-01-14 15:13:31 +0100

    trying to make archive work

---
+++ load.py
@@ -0,0 +1,17 @@
+from board import Board
+
+class Load_game():
+    def __init__(self):
+        pass
+    
+    def game(self, game):
+        board = Board()
+        if game <= 3:
+            board.load_cars(f"boards/Rushhour6x6_{game}.csv")
+        elif game <= 6: 
+            board.load_cars(f"boards/Rushhour9x9_{game}.csv")
+        else: 
+            board.load_cars(f"boards/Rushhour12x12_{game}.csv")
+        
+        return board
+       
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from sys import argv
 from cars import Cars
 from board import Board
 from solution import Solution
+from load import Load_game
 
 
 # Check command line arguments
@@ -19,14 +20,9 @@ elif len(argv) == 2:
 
 print("Welcome to Rush Hour!\n")
 
-# def load_game(self): 
-board = Board()
-if game_name <= 3:
-    board.load_cars(f"boards/Rushhour6x6_{game_name}.csv")
-elif game_name <= 6: 
-    board.load_cars(f"boards/Rushhour9x9_{game_name}.csv")
-else: 
-    board.load_cars(f"boards/Rushhour12x12_{game_name}.csv")
+load = Load_game()
+board = load.game(game_name)
+
 
 board.load_board()
 board.visualize_board()
@@ -35,16 +31,17 @@ winning_coordinate = board.board_size - 2
 
 red_car = board.cars['X']
 
-while board.arch_count < 5:
-    while red_car.col != winning_coordinate:
-        board.check_move()
-        board.move()
-        board.load_board()
-        board.visualize_board()
-        print("\n")
+while red_car.col != winning_coordinate:
+# for i in range(15):
+    board.check_move()
+    board.move()
+    board.load_board()
+    board.visualize_board()
+    print("\n")
         
+
 board.visualize_board()
 
 solve_game = Solution(game_name, board.all_moves)
 
-solve_game.solution()
+# solve_game.solution()
